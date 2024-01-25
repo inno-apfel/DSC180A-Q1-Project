585c8e3bc527506468e65d951de738615e5694fe
inno-apfel <mawei@ucsd.edu>
2024-01-25 05:57:18 -0800

    fixed to process mid career info

--- src/main/data_processing/retirement_processing.py
+++ src/main/data_processing/retirement_processing.py
@@ -1,44 +1,76 @@
 import numpy as np
 import pandas as pd
 
+
+    
+    # data = data.drop(columns=['total_population'])
+
+
 def process_retire_data(data, year, params):
     
     column_names = data.columns
     # Only getting Estimates
     filtered_columns = [col for col in column_names if col[-8:] == 'Estimate' and col[6:11].isdigit()]
+
     data = data[filtered_columns]
+
     # Cleaning up so that the rows are just the zipcode
     data.columns = data.columns.str.replace('ZCTA5 ', '').str.replace('!!Estimate', '')
+    
+    # Filter columns based on San Diego County zip codes
+
     # Selecting: Total population and retired aged
-    selected_rows = data.loc[[1, 14, 15, 16]]
+    selected_rows = data.loc[[1, 9, 10, 14, 15, 16]]
+
     # Define the new row names
     og_names = {
-                    1: 'Total population',
-                    14: '65 to 74 years',
-                    15: '75 to 84 years',
-                    16: '85 years and over'
-                }
+        1: 'Total population',
+        9: '25 to 34 years',
+        10: '35 to 44 years',
+        14: '65 to 74 years',
+        15: '75 to 84 years',
+        16: '85 years and over'
+    }
+
     data = selected_rows.rename(index=og_names)
+      
     # Remove commas from all numbers in all columns
     data = data.applymap(lambda x: str(x).replace(',', ''))
+
     # Convert all columns to numeric type
     data = data.apply(pd.to_numeric, errors='ignore')
-    # Transpose dataframe and convert zip-code col
-    data = data.T
-    data.index = data.index.set_names(['zip'])
-    data = data.reset_index()
-    data['zip'] = data['zip'].astype('int64')
-    # Creating row for total retirement population
-    data.insert(2, 'Total Retirement', data.iloc[:,2:].sum(axis=1))
+
+    # Creating row that is total retirement population
+    new_row = data.iloc[3] + data .iloc[4] + data.iloc[5]
+
+    new_row.name = 'Total Retirement'
+
+    # Append the new row to the DataFrame
+    data = data.append(new_row)
+    
+    # Creating row that is total mid career population
+    new_row = data.iloc[1] + data .iloc[2]
+
+    new_row.name = 'Total Mid Career'
+    
+    # Append the new row to the DataFrame
+    data = data.append(new_row)
+    
+    data = data.loc[['Total population', 'Total Retirement', 'Total Mid Career']]
+    
+    data = data.reset_index().T
+
     # Assign year variable
     data = data.assign(year=np.full(data.shape[0], year))
-    # Keeping only relevant zip-codes
-    data = data[data['zip'].apply(lambda x: x in params['zip_codes'])]
-    data = data.rename(columns={'Total population': 'total_population',
-                                'Total Retirement': 'total_retirement',
-                                '65 to 74 years': 'population_between_65_to_74',
-                                '75 to 84 years': 'population_between_75_to_84',
-                                '85 years and over': 'population_85_and_older'})
+    
+    data = data.drop(data.index[0])
+    
+    data = data.reset_index()
+    
+    data = data.rename(columns={0: 'total_population', 1: 'total_retirement', 2: 'total_mid_career', 'index': 'zip'})
+
     data = data.drop(columns=['total_population'])
+
+    data['zip'] = data['zip'].astype('int64').apply(lambda x: x in params['zip_codes'])
     
     return data
